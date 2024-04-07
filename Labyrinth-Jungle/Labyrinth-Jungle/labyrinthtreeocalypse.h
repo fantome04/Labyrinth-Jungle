e76412b301bdc4259e9be8581531d18d006ad95c
Anahit <anah.arakelyan@gmail.com>
2024-04-07 18:53:57 +0400

    added comments

--- Labyrinth-Jungle/Labyrinth-Jungle/labyrinthtreeocalypse.h
+++ Labyrinth-Jungle/Labyrinth-Jungle/labyrinthtreeocalypse.h
@@ -10,17 +10,17 @@ public:
 	LabyrinthTreeocalypse();
 	~LabyrinthTreeocalypse() = default;
 
-	virtual void generate_labyrinth() override;
-	virtual void plant_trees() override;
-	virtual void update_trees() override;
+	virtual void generate_labyrinth() override; //generate labyrinth 
+	virtual void plant_trees() override; //plants 3 trees in random places on the board
+	virtual void update_trees() override; //calls update method for all trees
 
 private:
-	void dfs(std::vector<std::vector<char>>& board_, const Coordinate& start);
-	int count_visited_neighbours(const std::vector<std::vector<char>>& board_, const Coordinate& start);
-	void shuffle(std::vector<int>& visit_order);
-	void generate_exits();
-	void compare_exits();
-	void starting_trees(); 
+	void dfs(std::vector<std::vector<char>>& board_, const Coordinate& start); //generate labyrinth with dfs
+	int count_visited_neighbours(const std::vector<std::vector<char>>& board_, const Coordinate& start); //helper function
+	void shuffle(std::vector<int>& visit_order); //helper function
+	void generate_exits(); //generates exit(s) on board
+	void compare_exits(); //compares exit locations with each other
+	void starting_trees(); //creates vector of trees at the start of the game
 };
 
 #endif // !TREEOCALYPSE_H
